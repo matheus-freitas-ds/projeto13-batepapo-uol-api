ecfe7afa444514219a0f32068548917aebc3819c
Matheus <matheusvmja@gmail.com>
2023-06-28 22:17:11 -0300

    Setup finished

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
 
 const app = express();
-app.use (cors());
-app.use (express.json());
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
-const PORT = 5000;
+const mongoClient = new MongoClient(process.env.DATABASE_URL); 
+let db;
+
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message));
 
 // LÓGICA DO BACK-END
 
-app.listen (PORT, () => (`O servidor está rodando na porta ${PORT}`));
+const PORT = 5000;
+app.listen(PORT, () => (`O servidor está rodando na porta ${PORT}`));
