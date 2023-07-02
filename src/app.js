55473f2dd19b71e96f234038ec51b1a57c59b36e
Matheus <matheusvmja@gmail.com>
2023-07-01 21:39:28 -0300

    Post/participants almost done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
+import joi from "joi";
 
 const app = express();
 app.use(cors());
@@ -16,6 +17,34 @@ mongoClient.connect()
     .catch((err) => console.log(err.message));
 
 // LÓGICA DO BACK-END
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+
+    const schemaParticipant = joi.object({
+        name: joi.string().required()
+    })
+
+    const validation = schemaParticipant.validate(req.body, { abortEarly: false })
+
+    if (validation.error){
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const participant = await db.collection("participants").findOne({ name: name})
+        if (participant) return res.status(409).send("This username is taken!")
+
+        await Promise.all([db.collection("participants").insertOne(req.body, { lastStatus: Date.now() }), db.collection("messages").insertOne({from: req.body, to: 'Todos', text: 'Entra na sala...', type: 'status', time:'HH:mm:ss' })])
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+})
+
+//FIM DA LÓGICA DO BACK-END
 
 const PORT = 5000;
 app.listen(PORT, () => (`O servidor está rodando na porta ${PORT}`));
